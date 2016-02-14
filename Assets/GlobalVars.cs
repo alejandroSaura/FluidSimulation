1bf13103f77fa83327bde302c601f1d1d690301d
alejandroSaura <alejandro.saura.villanueva@gmail.com>
2016-02-14 22:22:43 +0000

    Initial commit. Working on architectural grids system

---
+++ Assets/GlobalVars.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace global
+{
+    public static class GlobalVar
+    {
+        public const float FLT_EPSILON = 1.192092896e-07F;        /* smallest such that 1.0+FLT_EPSILON != 1.0 */
+        public const float Nudge = 1.0f + FLT_EPSILON;
+
+        
+    }
+
+    public static class ListExtra
+    {
+        public static void Resize<T>(List<T> list, int size, T element = default(T))
+        {
+            int count = list.Count;
+
+            if (size < count)
+            {
+                list.RemoveRange(size, count - size);
+            }
+            else if (size > count)
+            {
+                if (size > list.Capacity)   // Optimization
+                    list.Capacity = size;
+
+                list.AddRange(System.Linq.Enumerable.Repeat(element, size - count));
+            }
+        }
+    }
+
+
+}
+
